cb1a96c53460f00a13673d1e6d06876f420383df
TrentC <leesecurelee@proton.me>
2026-02-01 17:41:53 -0600

    Update init.lua
    forgot comma

--- init.lua
+++ init.lua
@@ -615,7 +615,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         --'lua_ls', -- Lua Language server
-        'lua-language-server' --working Lua Language server name, because Mason package names use kebab-case
+        'lua-language-server', --working Lua Language server name, because Mason package names use kebab-case
         'stylua', -- Used to format Lua code
         -- You can add other tools here that you want Mason to install
       })
